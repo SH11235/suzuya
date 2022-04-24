c5cb4b04120985a2f4af06979d0248488234b0f4
hiraoka <aabceh1a1a2i3u5o8@gmail.com>
2022-04-25 02:34:13 +0900

    feat create item_category page using "Tera" template

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,8 +1,113 @@
-// use entity::sea_orm;
+use actix_files::Files as Fs;
+use actix_web::{
+    error, get, middleware, post, web, App, Error, HttpRequest, HttpResponse, HttpServer, Result
+};
+// use entity::user;
+// use entity::user::Entity as User;
+use entity::item_category;
+use entity::item_category::Entity as ItemCategory;
+// use entity::maker;
+// use entity::maker::Entity as Maker;
+use entity::sea_orm;
+use listenfd::ListenFd;
+// use migration::{Migrator, MigratorTrait};
+use sea_orm::DatabaseConnection;
+use sea_orm::{entity::*, query::*};
+use serde::{Deserialize, Serialize};
+use std::env;
+use tera::Tera;
 
-pub use entity::user;
-pub use entity::user::Entity as User;
+const DEFAULT_ITEMS_PER_PAGE: usize = 5;
 
-fn main() {
-    print!("hello")
+#[derive(Debug, Clone)]
+struct AppState {
+    templates: Tera,
+    conn: DatabaseConnection,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Params {
+    page: Option<usize>,
+    items_per_page: Option<usize>,
+}
+
+#[get("/item_category")]
+async fn item_category_list(req: HttpRequest, data: web::Data<AppState>) -> Result<HttpResponse, Error> {
+    let template = &data.templates;
+    let conn = &data.conn;
+
+    // get params
+    let params = web::Query::<Params>::from_query(req.query_string()).unwrap();
+
+    let page = params.page.unwrap_or(1);
+    let items_per_page = params.items_per_page.unwrap_or(DEFAULT_ITEMS_PER_PAGE);
+    let paginator = ItemCategory::find()
+        .order_by_asc(item_category::Column::Id)
+        .paginate(conn, items_per_page);
+    let num_pages = paginator.num_pages().await.ok().unwrap();
+
+    let datas = paginator
+        .fetch_page(page - 1)
+        .await
+        .expect("could not retrieve datas");
+    let mut ctx = tera::Context::new();
+    let h1 = "アイテムカテゴリー";
+    let path = "item_category";
+    ctx.insert("datas", &datas);
+    ctx.insert("page", &page);
+    ctx.insert("h1", &h1);
+    ctx.insert("path", &path);
+    ctx.insert("items_per_page", &items_per_page);
+    ctx.insert("num_pages", &num_pages);
+
+    let body = template
+        .render("data.html.tera", &ctx)
+        .map_err(|_| error::ErrorInternalServerError("Template error"))?;
+    Ok(HttpResponse::Ok().content_type("text/html").body(body))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "debug");
+    tracing_subscriber::fmt::init();
+
+    // get env vars
+    dotenv::dotenv().ok();
+    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
+    let host = env::var("HOST").expect("HOST is not set in .env file");
+    let port = env::var("PORT").expect("PORT is not set in .env file");
+    let server_url = format!("{}:{}", host, port);
+
+    // establish connection to database and apply migrations
+    // -> create post table if not exists
+    let conn = sea_orm::Database::connect(&db_url).await.unwrap();
+    // Migrator::up(&conn, None).await.unwrap();
+
+    // load tera templates and build app state
+    let templates = Tera::new(concat!(env!("CARGO_MANIFEST_DIR"), "/templates/**/*")).unwrap();
+    let state = AppState { templates, conn };
+
+    // create server and try to serve over socket if possible
+    let mut listenfd = ListenFd::from_env();
+    let mut server = HttpServer::new(move || {
+        App::new()
+            .service(Fs::new("/static", "./static"))
+            .app_data(web::Data::new(state.clone()))
+            .wrap(middleware::Logger::default()) // enable logger
+            .configure(init)
+    });
+
+    server = match listenfd.take_tcp_listener(0)? {
+        Some(listener) => server.listen(listener)?,
+        None => server.bind(&server_url)?,
+    };
+
+    println!("Starting server at {}", server_url);
+    server.run().await?;
+
+    Ok(())
+}
+
+pub fn init(cfg: &mut web::ServiceConfig) {
+    cfg.service(item_category_list);
 }
