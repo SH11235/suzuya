ef905541ebdbdd8c3740cff156e5ecc627f05cf3
hiraoka <aabceh1a1a2i3u5o8@gmail.com>
2022-04-30 15:22:55 +0900

    chore サンプルデータ投入クエリ追加

--- sample_data.sql
+++ sample_data.sql
@@ -10,3 +10,12 @@ INSERT INTO makers (code_name) VALUES
     ('II'),
     ('JJ')
 ;
+
+INSERT INTO users (name, deleted, created_at, updated_at) VALUES
+    ('name1', 'false', '2000-01-01T00:00:00+09:00', '2000-01-01T00:00:00+09:00'),
+    ('name2', 'false', '2022-02-22T22:22:22+09:00', '2022-12-22T22:22:22+09:00')
+;
+
+INSERT INTO items (title, name, product_code,arrival_date, reservation_start_date, reservation_deadline, order_date, sku, illust_status, design_status, last_updated, retail_price, catalog_status, announcement_status, remarks, maker_id, pic_id, double_check_person_id) VALUES
+    ('title', 'name', null, null, null, null, null, null, '未着手(白)', '未着手(白)', '2022-02-22T22:22:22+09:00', null, '未着手(白)', '未着手(白)', null, 1, 1, null)
+;
