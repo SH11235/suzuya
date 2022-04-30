e06114800b22d58c1b16b6338cee66e95bf27bdc
hiraoka <aabceh1a1a2i3u5o8@gmail.com>
2022-05-01 02:29:12 +0900

    fix itemテーブルのカラム定義変更

--- entity/src/item.rs
+++ entity/src/item.rs
@@ -9,15 +9,16 @@ pub struct Model {
     pub title: String,
     pub name: String,
     pub product_code: Option<String>,
-    pub arrival_date: Option<Date>,           // メーカーからの入荷日
-    pub reservation_start_date: Option<Date>, // 予約開始日(BtoBおよびBtoC)
-    pub reservation_deadline: Option<Date>,   // 予約締切日
-    pub order_date: Option<Date>,             // メーカーへの発注日
+    pub release_date: Option<DateTimeLocal>,           // 発売日
+    pub arrival_date: Option<DateTimeLocal>,           // メーカーからの入荷日
+    pub reservation_start_date: Option<DateTimeLocal>, // 予約開始日(BtoBおよびBtoC)
+    pub reservation_deadline: Option<DateTimeLocal>,   // 予約締切日
+    pub order_date: Option<DateTimeLocal>,             // メーカーへの発注日
     pub sku: Option<i32>,                     // 種類数
     pub illust_status: String,
     pub design_status: String,
     pub last_updated: DateTimeLocal, // 最終更新日（ステータス変更時）
-    pub retail_price: Option<u32>,   // 上代
+    pub retail_price: Option<i32>,   // 上代
     pub catalog_status: String,
     pub announcement_status: String,
     pub remarks: Option<String>, // 備考
