19a48239d796d7e3b60df3f215a03b23952e4ee8
SH11235 <aabceh1a1a2i3u5o8@gmail.com>
2022-10-18 02:28:27 +0900

    refactor add title entity and update sea-orm

--- server/entity/src/lib.rs
+++ server/entity/src/lib.rs
@@ -1,4 +1,4 @@
 pub mod item;
 pub mod maker;
-pub mod user;
-pub use sea_orm;
+pub mod title;
+pub mod worker;
--- server/entity/src/title.rs
+++ server/entity/src/title.rs
@@ -4,11 +4,11 @@ use sea_orm::entity::prelude::*;
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, PartialEq, DeriveEntityModel, Deserialize, Serialize)]
-#[sea_orm(table_name = "maker")]
+#[sea_orm(table_name = "title")]
 pub struct Model {
     #[sea_orm(primary_key, auto_increment = false)]
     pub id: Uuid,
-    pub code_name: String,
+    pub name: String,
     pub deleted: bool,
 }
 
