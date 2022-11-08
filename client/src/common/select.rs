a5dda0558583cf1eb491f1035f2485f7a79904f0
SH11235 <aabceh1a1a2i3u5o8@gmail.com>
2022-11-09 02:18:33 +0900

    design color select box

--- client/src/common/select.rs
+++ client/src/common/select.rs
@@ -1,270 +1,220 @@
-// use std::collections::HashMap;
+use serde::Serialize;
+use std::fmt;
 
 use super::api::RESUBMISSION_OK;
 
-pub fn project_type_list() -> Vec<String> {
+pub fn project_type_list() -> Vec<StatusName> {
     vec![
-        String::from("デフォルト"),
-        String::from("S案件"),
-        String::from("Y案件"),
-        String::from("再販"),
+        StatusName {
+            name: String::from("デフォルト"),
+            color: Color::WHITE,
+        },
+        StatusName {
+            name: String::from("S案件"),
+            color: Color::YELLOW,
+        },
+        StatusName {
+            name: String::from("Y案件"),
+            color: Color::BLUE,
+        },
+        StatusName {
+            name: String::from("再販"),
+            color: Color::GRAY,
+        },
     ]
 }
 
-pub fn catalog_status_list() -> Vec<String> {
+pub fn catalog_status_list() -> Vec<StatusName> {
     vec![
-        String::from("未着手"),
-        String::from("不要"),
-        String::from("作成中"),
-        String::from("作成済み"),
+        StatusName {
+            name: String::from("未着手"),
+            color: Color::WHITE,
+        },
+        StatusName {
+            name: String::from("不要"),
+            color: Color::GRAY,
+        },
+        StatusName {
+            name: String::from("作成中"),
+            color: Color::YELLOW,
+        },
+        StatusName {
+            name: String::from("作成済み"),
+            color: Color::GREEN,
+        },
     ]
 }
 
-pub fn illust_status_list() -> Vec<String> {
+pub fn illust_status_list() -> Vec<StatusName> {
     vec![
-        String::from("未着手"),
-        String::from("素材提供待ち"),
-        String::from("ラフ作成中"),
-        String::from("ラフ監修中"),
-        String::from("イラスト納品待ち"),
-        String::from("線画作成中"),
-        String::from("線画監修中"),
-        String::from("着彩作成中"),
-        String::from("着彩監修中"),
-        String::from("完了"),
+        StatusName {
+            name: String::from("未着手"),
+            color: Color::WHITE,
+        },
+        StatusName {
+            name: String::from("素材提供待ち"),
+            color: Color::PINK,
+        },
+        StatusName {
+            name: String::from("ラフ作成中"),
+            color: Color::YELLOW,
+        },
+        StatusName {
+            name: String::from("ラフ監修中"),
+            color: Color::PINK,
+        },
+        StatusName {
+            name: String::from("イラスト納品待ち"),
+            color: Color::PINK,
+        },
+        StatusName {
+            name: String::from("線画作成中"),
+            color: Color::YELLOW,
+        },
+        StatusName {
+            name: String::from("線画監修中"),
+            color: Color::PINK,
+        },
+        StatusName {
+            name: String::from("着彩作成中"),
+            color: Color::YELLOW,
+        },
+        StatusName {
+            name: String::from("着彩監修中"),
+            color: Color::PINK,
+        },
+        StatusName {
+            name: String::from("完了"),
+            color: Color::GREEN,
+        },
     ]
 }
 
-pub fn design_status_list() -> Vec<String> {
+pub fn design_status_list() -> Vec<StatusName> {
     vec![
-        String::from("未着手"),
-        String::from("素材提供待ち"),
-        String::from("デザイン作成中"),
-        String::from("デザイン監修中"),
-        String::from("入稿データ作成中"),
-        String::from("校正手配中"),
-        String::from("他校正待ち"),
-        String::from("校正監修中"),
-        String::from("校了"),
-        String::from("発注済み"),
+        StatusName {
+            name: String::from("未着手"),
+            color: Color::WHITE,
+        },
+        StatusName {
+            name: String::from("素材提供待ち"),
+            color: Color::PINK,
+        },
+        StatusName {
+            name: String::from("デザイン作成中"),
+            color: Color::YELLOW,
+        },
+        StatusName {
+            name: String::from("デザイン監修中"),
+            color: Color::PINK,
+        },
+        StatusName {
+            name: String::from("入稿データ作成中"),
+            color: Color::YELLOW,
+        },
+        StatusName {
+            name: String::from("校正手配中"),
+            color: Color::BLUE,
+        },
+        StatusName {
+            name: String::from("他校正待ち"),
+            color: Color::YELLOW,
+        },
+        StatusName {
+            name: String::from("校正監修中"),
+            color: Color::PINK,
+        },
+        StatusName {
+            name: String::from("校了"),
+            color: Color::YELLOW,
+        },
+        StatusName {
+            name: String::from("発注済み"),
+            color: Color::GREEN,
+        },
     ]
 }
 
-pub fn resubmission_list() -> Vec<String> {
+pub fn resubmission_list() -> Vec<StatusName> {
     vec![
-        String::from(""),
-        String::from(RESUBMISSION_OK),
+        StatusName {
+            name: String::from(""),
+            color: Color::WHITE,
+        },
+        StatusName {
+            name: String::from(RESUBMISSION_OK),
+            color: Color::YELLOW,
+        },
     ]
 }
 
-pub fn line_list() -> Vec<String> {
+pub fn line_list() -> Vec<StatusName> {
     vec![
-        String::from("未対応"),
-        String::from("不要"),
-        String::from("対応済"),
+        StatusName {
+            name: String::from("未対応"),
+            color: Color::YELLOW,
+        },
+        StatusName {
+            name: String::from("不要"),
+            color: Color::WHITE,
+        },
+        StatusName {
+            name: String::from("対応済"),
+            color: Color::GREEN,
+        },
     ]
 }
 
-pub fn announce_status_list() -> Vec<String> {
+pub fn announce_status_list() -> Vec<StatusName> {
     vec![
-        String::from("未着手"),
-        String::from("不要"),
-        String::from("作成中"),
-        String::from("監修中"),
-        String::from("作成済み"),
+        StatusName {
+            name: String::from("未着手"),
+            color: Color::WHITE,
+        },
+        StatusName {
+            name: String::from("不要"),
+            color: Color::GRAY,
+        },
+        StatusName {
+            name: String::from("作成中"),
+            color: Color::YELLOW,
+        },
+        StatusName {
+            name: String::from("監修中"),
+            color: Color::PINK,
+        },
+        StatusName {
+            name: String::from("作成済み"),
+            color: Color::GREEN,
+        },
     ]
 }
 
-// pub fn color_list() -> Vec<String> {
-//     vec![
-//         String::from("white"),
-//         String::from("pink"),
-//         String::from("yellow"),
-//         String::from("light-blue"),
-//         String::from("green"),
-//         String::from("gray"),
-//     ]
-// }
+#[derive(Clone, Debug, PartialEq, Eq, Serialize)]
+pub enum Color {
+    WHITE,
+    PINK,
+    YELLOW,
+    BLUE,
+    GREEN,
+    GRAY,
+}
 
-// pub fn project_type_color_map() -> HashMap<String, String> {
-//     let mut map = HashMap::new();
-//     map.insert(String::from("デフォルト"), String::from("white"));
-//     map.insert(String::from("S案件"), String::from("yellow"));
-//     map.insert(String::from("Y案件"), String::from("light-blue"));
-//     map.insert(String::from("再販"), String::from("gray"));
-//     map
-// }
+impl fmt::Display for Color {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Color::WHITE => write!(f, "white"),
+            Color::PINK => write!(f, "pink"),
+            Color::YELLOW => write!(f, "yellow"),
+            Color::BLUE => write!(f, "blue"),
+            Color::GREEN => write!(f, "green"),
+            Color::GRAY => write!(f, "gray"),
+        }
+    }
+}
 
-// #[derive(Debug, Serialize, Deserialize)]
-// pub struct StatusName {
-//     pub name: String,
-//     pub color: Color,
-// }
-
-// #[derive(Debug, Serialize, Deserialize)]
-// pub enum Color {
-//     WHITE,
-//     PINK,
-//     YELLOW,
-//     LIGHTBLUE,
-//     GREEN,
-//     GRAY,
-// }
-
-// pub fn project_type_list() -> Vec<StatusName> {
-//     vec![
-//         StatusName {
-//             name: String::from("デフォルト"),
-//             color: Color::WHITE,
-//         },
-//         StatusName {
-//             name: String::from("S案件"),
-//             color: Color::YELLOW,
-//         },
-//         StatusName {
-//             name: String::from("Y案件"),
-//             color: Color::LIGHTBLUE,
-//         },
-//         StatusName {
-//             name: String::from("再販"),
-//             color: Color::GRAY,
-//         },
-//     ]
-// }
-
-// pub fn illust_status_list() -> Vec<StatusName> {
-//     vec![
-//         StatusName {
-//             name: String::from("未着手"),
-//             color: Color::WHITE,
-//         },
-//         StatusName {
-//             name: String::from("素材提供待ち"),
-//             color: Color::PINK,
-//         },
-//         StatusName {
-//             name: String::from("ラフ作成中"),
-//             color: Color::YELLOW,
-//         },
-//         StatusName {
-//             name: String::from("ラフ監修中"),
-//             color: Color::PINK,
-//         },
-//         StatusName {
-//             name: String::from("イラスト納品待ち"),
-//             color: Color::PINK,
-//         },
-//         StatusName {
-//             name: String::from("線画作成中"),
-//             color: Color::YELLOW,
-//         },
-//         StatusName {
-//             name: String::from("線画監修中"),
-//             color: Color::PINK,
-//         },
-//         StatusName {
-//             name: String::from("着彩作成中"),
-//             color: Color::YELLOW,
-//         },
-//         StatusName {
-//             name: String::from("着彩監修中"),
-//             color: Color::PINK,
-//         },
-//         StatusName {
-//             name: String::from("完了"),
-//             color: Color::GREEN,
-//         },
-//     ]
-// }
-
-// pub fn design_status_list() -> Vec<StatusName> {
-//     vec![
-//         StatusName {
-//             name: String::from("未着手"),
-//             color: Color::WHITE,
-//         },
-//         StatusName {
-//             name: String::from("素材提供待ち"),
-//             color: Color::PINK,
-//         },
-//         StatusName {
-//             name: String::from("デザイン作成中"),
-//             color: Color::YELLOW,
-//         },
-//         StatusName {
-//             name: String::from("デザイン監修中"),
-//             color: Color::PINK,
-//         },
-//         StatusName {
-//             name: String::from("入稿データ作成中"),
-//             color: Color::YELLOW,
-//         },
-//         StatusName {
-//             name: String::from("校正手配中"),
-//             color: Color::LIGHTBLUE,
-//         },
-//         StatusName {
-//             name: String::from("他校正待ち"),
-//             color: Color::YELLOW,
-//         },
-//         StatusName {
-//             name: String::from("校正監修中"),
-//             color: Color::PINK,
-//         },
-//         StatusName {
-//             name: String::from("校了"),
-//             color: Color::YELLOW,
-//         },
-//         StatusName {
-//             name: String::from("発注済み"),
-//             color: Color::GREEN,
-//         },
-//     ]
-// }
-
-// pub fn catalog_status_list() -> Vec<StatusName> {
-//     vec![
-//         StatusName {
-//             name: String::from("未着手"),
-//             color: Color::WHITE,
-//         },
-//         StatusName {
-//             name: String::from("不要"),
-//             color: Color::GRAY,
-//         },
-//         StatusName {
-//             name: String::from("作成中"),
-//             color: Color::YELLOW,
-//         },
-//         StatusName {
-//             name: String::from("作成済み"),
-//             color: Color::GREEN,
-//         },
-//     ]
-// }
-
-// pub fn announce_status_list() -> Vec<StatusName> {
-//     vec![
-//         StatusName {
-//             name: String::from("未着手"),
-//             color: Color::WHITE,
-//         },
-//         StatusName {
-//             name: String::from("不要"),
-//             color: Color::GRAY,
-//         },
-//         StatusName {
-//             name: String::from("作成中"),
-//             color: Color::YELLOW,
-//         },
-//         StatusName {
-//             name: String::from("監修中"),
-//             color: Color::PINK,
-//         },
-//         StatusName {
-//             name: String::from("作成済み"),
-//             color: Color::GREEN,
-//         },
-//     ]
-// }
-
+#[derive(Clone, Debug, PartialEq, Eq, Serialize)]
+pub struct StatusName {
+    pub name: String,
+    pub color: Color,
+}
--- client/src/components/common/select_box.rs
+++ client/src/components/common/select_box.rs
@@ -1,3 +1,4 @@
+use crate::common::select::StatusName;
 use yew::{events::Event, function_component, html, Callback, Html, Properties};
 
 #[derive(Properties, PartialEq)]
@@ -5,7 +6,7 @@ pub struct SelectBoxProperty {
     pub onchange: Callback<Event>,
     pub id: String,
     pub name: String,
-    pub select_list: Vec<String>,
+    pub select_list: Vec<StatusName>,
     pub value: String,
 }
 
@@ -13,7 +14,7 @@ pub struct SelectBoxProperty {
 pub fn select_box(props: &SelectBoxProperty) -> Html {
     let mut first_item = true;
     html! {
-        <select class="item-edit-input"
+        <select class="select-list"
             onchange = { props.onchange.clone() }
             id={ props.id.clone() } name={ props.name.clone() } >
         {
@@ -21,17 +22,17 @@ pub fn select_box(props: &SelectBoxProperty) -> Html {
                 if first_item && props.value.clone() == "".to_string() {
                     first_item = false;
                     html! {
-                        <option value={ option.clone() } selected=true>{ option.clone() }</option>
+                        <option class={ option.color.clone().to_string() } value={ option.name.clone() } selected=true>{ option.name.clone() }</option>
                     }
-                } else if props.value.clone() == option {
+                } else if props.value.clone() == option.name {
                     first_item = false;
                     html! {
-                        <option value={ option.clone() } selected=true>{ option.clone() }</option>
+                        <option class={ option.color.clone().to_string() } value={ option.name.clone() } selected=true>{ option.name.clone() }</option>
                     }
                 } else {
                     first_item = false;
                     html! {
-                        <option value={ option.clone() }>{ option.clone() }</option>
+                        <option class={ option.color.clone().to_string() } value={ option.name.clone() }>{ option.name.clone() }</option>
                     }
                 }
             }).collect::<Html>()
